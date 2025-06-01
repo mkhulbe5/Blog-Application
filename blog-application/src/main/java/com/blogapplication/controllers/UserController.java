74d40334acf8da6d7d19b9cfce56aebbf29df7d0
mkhulbe5 <chandmohit163@gmail.com>
2025-06-01 23:02:12 +0530

    setting up environment profiles and swagger doc

--- blog-application/src/main/java/com/blogapplication/controllers/UserController.java
+++ blog-application/src/main/java/com/blogapplication/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,12 +50,12 @@ public class UserController {
 //		return ResponseEntity.ok(response);
 //	}
 //
-//	@PostMapping("/create")
-//	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
-//		UserDto user = userService.createUser(userDto);
-//		log.info("user has been saved successfully");
-//		return new ResponseEntity<UserDto>(user, HttpStatus.CREATED);
-//	}
+	@PostMapping("/create")
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
+		UserDto user = userService.createUser(userDto);
+		log.info("user has been saved successfully");
+		return new ResponseEntity<UserDto>(user, HttpStatus.CREATED);
+	}
 
 	@PutMapping("{userId}")
 	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable Integer userId) {
